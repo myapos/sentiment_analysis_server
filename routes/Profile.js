1ca8d1adc9a7570380fe86e9bf92d3c1e1a9abeb
Myron <myron.apostolakis@onapp.com>
2021-05-16 13:03:03 +0300

    check 6

--- routes/Profile.js
+++ routes/Profile.js
@@ -12,7 +12,7 @@ const Profile = (app) => {
         maxAge: 30 * 60 * 1000, // 1/2 hour
         httpOnly: false,
         //   secure: false,
-        domain: 'herokuapp.com',
+        domain: 'sentimental-analysis-dashboard.herokuapp.com/',
         sameSite: true,
       });
 
